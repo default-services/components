2f341c980e9a671998b067ac1488e836f5ea2d92
unknown <wade.daniel.w@gmail.com>
2020-05-28 21:33:00 -0700

    adding PostCSS plugins to rollup config (e.g., autoprefix, preset-env)

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,14 @@
 import alias from '@rollup/plugin-alias';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
 
 const path = require('path');
 
+// Post CSS plugins
+const postcssPresetEnv = require('postcss-preset-env');
+const autoprefixer = require('autoprefixer');
 
 export default {
   input: 'src/index.js',
@@ -31,6 +34,10 @@ export default {
     postcss({
       extract: false,
       modules: true,
+      plugins: [
+        autoprefixer(),
+        postcssPresetEnv({ stage: 0 })
+      ],
       use: [ 'sass' ]
     }),
     babel({
