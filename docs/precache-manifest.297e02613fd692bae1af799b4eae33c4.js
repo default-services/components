d3030af0468a59ff2e134ee3b988154252935325
unknown <wade.daniel.w@gmail.com>
2020-05-28 18:24:14 -0700

    updating Storybook static build

---
+++ docs/precache-manifest.297e02613fd692bae1af799b4eae33c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1b031595ae66f439264ccdb27413bf",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.89de65bcb11ac10cabf1.bundle.js"
+  },
+  {
+    "url": "runtime~main.89de65bcb11ac10cabf1.bundle.js"
+  },
+  {
+    "revision": "4f82e79fe18c8cfabd87",
+    "url": "static/css/main.03f9e293.chunk.css"
+  },
+  {
+    "url": "vendors~main.89de65bcb11ac10cabf1.bundle.js"
+  },
+  {
+    "url": "vendors~main.89de65bcb11ac10cabf1.bundle.js.LICENSE.txt"
+  }
+]);
