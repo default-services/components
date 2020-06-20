b9977ccf12b765cedffc9a5ba4a6fdbd20503189
unknown <wade.daniel.w@gmail.com>
2020-06-20 00:01:46 -0700

    updated pagination component to be class-based

--- src/components/pagination/PaginationList.js
+++ src/components/pagination/PaginationList.js
@@ -1,96 +1,98 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { Component, createRef } from 'react';
 
 import PropTypes from 'prop-types';
 import setClassName from 'utilities/setClassName';
 
-// TODO: update to class-based component for CRL to work properly
-export const PaginationList = props => {
-  const { results = 10 } = props;
-  const [ currentResultsPage, setCurrentResultsPage ] = useState(0);
+export class PaginationList extends Component {
 
-  // All changed in useEffect, so grouped together
-  const [ pages, setPages ] = useState({
+  state = {
     active: false,
+    currentResultsPage: 0,
+    list: {},
     offsetHeight: 0,
-    list: {}
-  });
-
-  // Reference to current page
-  const currentPage = pages.list[currentResultsPage];
-
-  // List provided by user
-  const userList = useRef(PaginationList);
-  const UserList = providedListProps => {
-
-    // Filter out unneeded props before they are passed to the <ul>
-    const defaultListProps = (() => {
-      const output = { ...props };
-      delete output.results;
-      delete output.children;
-      return output;
-    })();
-
-    return (
-      <ul { ...defaultListProps } { ...providedListProps } ref={ userList }>
-        { currentPage }
-      </ul>
-    );
+    userList: createRef(PaginationList)
   };
 
-
-  // Numbered options to change the page
-  const PageOptions = () => {
-
-    return (
-      <article>
-        {
-          Object.keys(pages.list).map((page, index) => {
-            const handlePageChange = () => setCurrentResultsPage(index);
-
-            return (
-              <span key={ index } onClick={ handlePageChange }>
-                { index + 1 }
-              </span>
-            );
-          })
-        }
-      </article>
-    );
-  };
-
-
-  useEffect(() => {
+  // TODO: change to "getDerivedStateFromProps"
+  componentDidMount() {
 
     // Separate pages into corresponding arrays within an object
-    const resultPages = props.children.reduce((accumulator, child, index) => {
-      const current = Math.floor(index / results);
+    const resultPages = this.props.children.reduce((accumulator, child, index) => {
+      const current = Math.floor(index / (this.props.results || 10));
       accumulator[current] ? accumulator[current] = [ accumulator[current], child] : accumulator[current] = child;
       return accumulator;
     }, {});
 
-    const offsetHeight = userList.current.offsetHeight;
-    if(offsetHeight > 0 && !pages.active)
-      setPages({
-        ...pages,
-        offsetHeight,
-        active: true,
-        list: resultPages
-      });
-
-    else
-      setPages({ ...pages, list: resultPages });
-
-  }, [userList.current.offsetHeight]);
+    this.setState({ list: resultPages });
+  };
 
 
-  return (
-    <section className={ setClassName(props, 'pagination-list') }>
-      <UserList style={ { minHeight: pages.offsetHeight } } />
-      <PageOptions />
-    </section>
-  );
+  render() {
 
-};
+    // Reference to current page
+    const currentPage = this.state.list[this.state.currentResultsPage];
+
+    // List provided by user
+    const UserList = providedListProps => {
+
+      // Filter out unneeded props before they are passed to the <ul>
+      const defaultListProps = (() => {
+        const output = { ...this.props };
+        delete output.results;
+        delete output.children;
+        return output;
+      })();
+
+      return (
+        <ul { ...defaultListProps } { ...providedListProps } ref={ this.state.userList }>
+          { currentPage }
+        </ul>
+      );
+    };
+
+
+    // Numbered options to change the page
+    const PageOptions = () => {
+
+      return (
+        <article>
+          {
+            Object.keys(this.state.list).map((page, index) => {
+              const handlePageChange = () => {
+                const offsetHeight = this.state.userList.current.offsetHeight;
+
+                if(offsetHeight > 0 && !this.state.active)
+                  this.setState({
+                    offsetHeight,
+                    active: true,
+                    currentResultsPage: index
+                  });
+
+                else
+                  this.setState({ currentResultsPage: index });
+              };
+
+              return (
+                <span key={ index } onClick={ handlePageChange }>
+                  { index + 1 }
+                </span>
+              );
+            })
+          }
+        </article>
+      );
+    };
+
+
+    return (
+      <section className={ setClassName(this.props, 'pagination-list') }>
+        <UserList style={ { minHeight: this.state.offsetHeight } } />
+        <PageOptions />
+      </section>
+    );
+
+  };
+}
 
 
 PaginationList.propTypes = {
