276555ea77a0b13746dcfe5cdc40a3a449c51728
unknown <wade.daniel.w@gmail.com>
2020-07-03 21:32:18 -0700

    updates to Avatar component

---
+++ docs/precache-manifest.9b569ef047ae9f979eb43f94194ff062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10d127c5cd095f141a98323ce5bb5a5",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.307a1c8842c8747ef601.bundle.js"
+  },
+  {
+    "url": "runtime~main.307a1c8842c8747ef601.bundle.js"
+  },
+  {
+    "revision": "747083e0e4add345f55e",
+    "url": "static/css/main.8d89cdc6.chunk.css"
+  },
+  {
+    "url": "vendors~main.307a1c8842c8747ef601.bundle.js"
+  },
+  {
+    "url": "vendors~main.307a1c8842c8747ef601.bundle.js.LICENSE.txt"
+  }
+]);
