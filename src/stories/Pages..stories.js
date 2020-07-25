a3907e7c06c94ab55a01b3f8678cd46c5d49fb5c
dragon3hgmx <dragon3h@gmx.com>
2020-07-25 11:10:46 -0400

    added new component: Page not found

---
+++ src/stories/Pages..stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Page404 } from 'components/pages/Page404';
+
+export default {
+  title: 'Pages',
+  component: Page404
+};
+
+export const Page_404 = () => (
+  <Page404  />
+);
