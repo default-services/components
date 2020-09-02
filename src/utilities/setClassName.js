fb66270ed5eb436fdc553a342b70247758784d23
unknown <wade.daniel.w@gmail.com>
2020-09-01 20:03:07 -0700

    updating JSDoc documentation

--- src/utilities/setClassName.js
+++ src/utilities/setClassName.js
@@ -1,6 +1,11 @@
 import styles from 'src/assets/scss/styles.module.scss';
 
-// Allow for variants, and additional user-specified classes
+/**
+ * @description - Allow for variants, and additional user-specified classes.
+ * @property {Object} props - Component props.
+ * @property {string} name - Class name to use (which will be processed by CSS Modules).
+ */
+
 const setClassName = (props, name) => {
   const { className } = props;
   return className ? `${styles[name]} ${className}` : styles[name];
